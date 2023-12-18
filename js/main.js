584385a416cf1627c06031a6139920bd8c59419b
Noelia-07 <146656460+Noelia-07@users.noreply.github.com>
2023-12-18 13:28:03 -0300

    Update main-comentarios

--- js/main.js
+++ js/main.js
@@ -10,23 +10,16 @@ aria-expanded="false" aria-label="Toggle navigation">
 <div class="collapse navbar-collapse" id="collapsibleNavId">
 <ul class="navbar-nav me-auto mt-2 mt-lg-0">
 <li class="nav-item">
-<a class="nav-link active" href="index.html" aria-current="page">Home <span
+<a class="nav-link active" href="index.html" aria-current="page">INICIO<span
 
 class="visually-hidden">(current)</span></a>
 
 </li>
 <li class="nav-item">
-<a class="nav-link" href="#">Link</a>
 </li>
-<li class="nav-item dropdown">
-<a class="nav-link dropdown-toggle" href="#" id="dropdownId"
-data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
-<div class="dropdown-menu" aria-labelledby="dropdownId">
-<a class="dropdown-item" href="productos.html">Produtos</a>
-<a class="dropdown-item" href="#">Action 2</a>
-</div>
-</li>
+
 </ul>
+
 <form class="d-flex my-2 my-lg-0">
 <input class="form-control me-sm-2" type="text" placeholder="Search">
 <button class="btn btn-outline-success my-2 my-sm-0"
